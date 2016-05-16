3d8fda22a90b3ad2846f920b5391eb76ed2b7318
Ming Dai <radaiming@gmail.com>
2016-05-17 01:36:32 +0800

    Add new sample to demo way to use redirect

---
+++ samples/redirect.go
@@ -0,0 +1,17 @@
+/*
+Created © 2016-05-17 01:26 by @radaiming
+*/
+
+package main
+
+import "github.com/radaiming/dumpling"
+
+func redir() (int, map[string]string, string) {
+	headers := map[string]string{"Location": "https://google.com"}
+	return 301, headers, ""
+}
+
+func main() {
+	dumpling.AddRoute("/", "GET", redir)
+	dumpling.Serve("127.0.0.1:9988")
+}
