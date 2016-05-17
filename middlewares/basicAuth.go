9c45c2ac40e274a0ded71a94e6524f89eee820c2
Ming Dai <radaiming@gmail.com>
2016-05-18 02:15:56 +0800

    Add a middleware to do basic auth checking

---
+++ middlewares/basicAuth.go
@@ -0,0 +1,16 @@
+package middlewares
+
+import "net/http"
+
+func BasicAuth(username string, password string, h http.Handler) http.Handler {
+	// https://golang.org/src/net/http/request.go?s=20802:20868#L633
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		reqUsername, reqPassword, ok := r.BasicAuth()
+		if !ok || reqUsername != username || reqPassword != password {
+			w.WriteHeader(401)
+			w.Write([]byte("Unauthorized"))
+		} else {
+			h.ServeHTTP(w, r)
+		}
+	})
+}
