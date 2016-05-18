62586ed2ecb7250f56e4ec4fae75dc913c7cecb1
Ming Dai <radaiming@gmail.com>
2016-05-18 17:08:36 +0800

    Too much code in ServeHTTP, move some of them into a new func

--- router.go
+++ router.go
@@ -39,21 +39,24 @@ func (r *Router) Plug(h http.Handler) {
 	r.chainedMiddlewares = h
 }
 
+func initContext(ctx *HTTPContext, req *http.Request) {
+	ctx.reqHeaders = req.Header
+	ctx.reqArgs, _ = url.ParseQuery(req.URL.RawQuery)
+	if req.Method == "POST" {
+		if req.Header.Get("Content-Type") == "application/x-www-form-urlencoded" {
+			req.ParseForm()
+			ctx.postForm = req.PostForm
+		}
+	}
+}
+
 func (r *Router) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
 	f, ok := r.handlersMap[req.URL.Path][req.Method]
 	if !ok {
 		writer.WriteHeader(http.StatusNotFound)
 	} else {
 		ctx := newHTTPContext()
-		ctx.reqHeaders = req.Header
-		ctx.reqArgs, _ = url.ParseQuery(req.URL.RawQuery)
-		if req.Method == "POST" {
-			if req.Header.Get("Content-Type") == "application/x-www-form-urlencoded" {
-				req.ParseForm()
-				ctx.postForm = req.PostForm
-			}
-		}
-
+		initContext(ctx, req)
 		f(ctx)
 		for k, v := range ctx.respHeaders {
 			writer.Header().Set(k, v)
