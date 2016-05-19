fd41e3bd8bac5b1fbf3a18cfbda689fb264f7852
Ming Dai <radaiming@gmail.com>
2016-05-19 18:36:52 +0800

    Update sample for previous change

--- samples/static.go
+++ samples/static.go
@@ -5,9 +5,14 @@ import (
 	"github.com/radaiming/dumpling/middlewares"
 )
 
+func hello(ctx *dumpling.HTTPContext) {
+	ctx.Response("hello world")
+}
+
 func main() {
 	r := dumpling.New()
-	chainedMiddleware := middlewares.ServeStatic("/static", "/tmp/")
+	chainedMiddleware := middlewares.ServeStatic("/static", "/tmp/", r)
 	r.Plug(chainedMiddleware)
+	r.Post("/static", hello)
 	r.Serve("127.0.0.1:9988")
 }
