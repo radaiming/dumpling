edb6357513f80913365c0be67b7420ad65fe1ad8
Ming Dai <radaiming@gmail.com>
2016-05-20 18:17:51 +0800

    Add http_test.go, this test is as boring as dumpling_test.go

---
+++ http_test.go
@@ -0,0 +1,55 @@
+package dumpling
+
+import (
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+func TestSetStatusCode(t *testing.T) {
+	c := newHTTPContext()
+	c.SetStatusCode(500)
+	if c.respStatusCode != 500 {
+		t.Error("Failed to set resopnse status code")
+	}
+}
+
+func TestAddHeader(t *testing.T) {
+	c := newHTTPContext()
+	c.AddHeader("Content-Type", "plain/text")
+	if c.respHeaders["Content-Type"] != "plain/text" {
+		t.Error("Failed to add response HTTP Header")
+	}
+}
+
+func TestGetHeader(t *testing.T) {
+	c := newHTTPContext()
+	headers := http.Header{}
+	headers.Add("Content-Type", "application/xml")
+	c.reqHeaders = headers
+	if c.GetHeader("Content-Type") != "application/xml" {
+		t.Error("Failed to get request HTTP header")
+	}
+}
+
+func TestResponse(t *testing.T) {
+	c := newHTTPContext()
+	c.Response("blabla")
+	if c.respContent != "blabla" {
+		t.Error("Failed to set response content")
+	}
+}
+
+func TestGetReqArgs(t *testing.T) {
+	c := newHTTPContext()
+	args := url.Values{}
+	args.Add("a", "b")
+	args.Add("c", "d")
+	c.reqArgs = args
+	if len(c.GetReqArgs()) != len(args) || c.GetReqArgs()["a"][0] != "b" || c.GetReqArgs()["c"][0] != "d" {
+		t.Error("GetReqArgs() returns different values")
+	}
+	if c.GetReqArg("a") != "b" || c.GetReqArg("c") != "d" {
+		t.Error("GetReqArg() returns different value")
+	}
+}
