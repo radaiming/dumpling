1e1b6d1a61563d35d7f550e0e10c03e456962b7e
Ming Dai <radaiming@gmail.com>
2016-05-20 20:57:51 +0800

    Use http.ServeFile for serving static file

--- middlewares/serveStatic.go
+++ middlewares/serveStatic.go
@@ -1,9 +1,7 @@
 package middlewares
 
 import (
-	"io"
 	"net/http"
-	"os"
 	"path/filepath"
 	"strings"
 )
@@ -23,30 +21,6 @@ func ServeStatic(urlPath string, fsPath string, h http.Handler) http.Handler {
 			panic(err)
 		}
 		absReqPath := filepath.Join(absFsPath, relPathToUrlPath)
-		f, err := os.Open(absReqPath)
-		if err != nil {
-			if os.IsNotExist(err) {
-				w.WriteHeader(404)
-			} else if os.IsPermission(err) {
-				w.WriteHeader(403)
-			} else {
-				w.WriteHeader(400)
-			}
-			return
-		}
-		defer f.Close()
-		fInfo, err := f.Stat()
-		if err != nil {
-			// os in Go is too tedious compared to Py...
-			w.WriteHeader(400)
-			return
-		}
-		if !fInfo.Mode().IsRegular() {
-			// only serve regular file, nginx return
-			// 200 by default when requesting directory
-			w.WriteHeader(200)
-			return
-		}
-		io.Copy(w, f)
+		http.ServeFile(w, r, absReqPath)
 	})
 }
